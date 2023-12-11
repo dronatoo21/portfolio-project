26c92d303504906e42c9fd52643ac30fc0cd12fa
dronatoo <mrdronatoo@gmail.com>
2023-12-11 09:17:27 -0800

    added new options in navbar

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -5,16 +5,18 @@ const Navbar = () => {
     const links = <>
         <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/">Home</NavLink></li>
         {/* <li><Link className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} activeClass="active" to="test1" spy={true} smooth={true} offset={50} duration={500} onSetActive={'handleSetActive'}>Home</Link></li> */}
-        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/skills">Skills</NavLink></li>
-        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/blogs">Blog</NavLink></li>
         <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/projects">Projects</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/skills">Skills</NavLink></li>
         <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/experience">Experience</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/education">Education</NavLink></li>
         <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/about">About</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/blogs">Blog</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/contact">Contact</NavLink></li>
     </>
     return (
         <div>
-            <div className="navbar bg-[#3c3c3c] text-white px-20 py-5 rounded-b-full">
-              <div className="navbar-start w-full items-center">
+            <div className="navbar bg-[#3c3c3c] text-white px-20 flex justify-between py-5 rounded-b-full">
+              <div className="w-full md:w-auto items-center">
                 <div className="dropdown">
                   <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                     <FaBars className="text-base md:text-lg"/>
@@ -25,7 +27,7 @@ const Navbar = () => {
                 </div>
                 <h1 className="text-base md:text-2xl md:w-auto font-semibold">SHEFA ISLAM FAHIM</h1>
               </div>
-              <div className="navbar-end lg:block hidden">
+              <div className="lg:block hidden">
                 <ul className="menu menu-horizontal px-1 text-white">
                     {links}
                 </ul>
