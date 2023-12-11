67844bc144268bb44201f8178eb19a4a92557bdd
dronatoo <mrdronatoo@gmail.com>
2023-12-11 09:07:45 -0800

    completed navbar and banner section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,8 +3,13 @@ import programmer from "../../../assets/programmer.json"
 
 const Banner = () => {
     return (
-        <div className="flex justify-between items-center">
-            <div>Fahim</div>
+        <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+            <div className="text-white md:pl-20 space-y-2 pb-14 text-center md:text-start">
+                <h1 className="md:text-3xl lg:text-5xl font-bold ">I AM FAHIM</h1>
+                <h3 className="md:text-2xl lg:text-3xl pb-5 md:pb-5 lg:pb-10">Front End Web Developer</h3>
+                <button className="btn bg-[#c8d5e5] text-[#3c3c3c] border-none">HIRE ME</button>
+                <button className="btn bg-[#fa3433] text-white border-none ml-3">RESUME</button>
+            </div>
             <Lottie className="w-3/6" animationData={programmer}/>
         </div>
     );
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,25 +1,32 @@
+import { NavLink } from "react-router-dom";
+import { FaBars } from 'react-icons/fa';
+
 const Navbar = () => {
     const links = <>
-        <li><a>Item 1</a></li>
-        <li><a>Item 2</a></li>
-        <li><a>Item 3</a></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/">Home</NavLink></li>
+        {/* <li><Link className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} activeClass="active" to="test1" spy={true} smooth={true} offset={50} duration={500} onSetActive={'handleSetActive'}>Home</Link></li> */}
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/skills">Skills</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/blogs">Blog</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/projects">Projects</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/experience">Experience</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/about">About</NavLink></li>
     </>
     return (
         <div>
-            <div className="navbar bg-[#03112b] text-white">
-              <div className="navbar-start">
+            <div className="navbar bg-[#3c3c3c] text-white px-20 py-5 rounded-b-full">
+              <div className="navbar-start w-full items-center">
                 <div className="dropdown">
                   <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    <FaBars className="text-base md:text-lg"/>
                   </div>
-                  <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                  <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 text-black rounded-box w-52">
                     {links}
                   </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">SHEFA ISLAM FAHIM</a>
+                <h1 className="text-base md:text-2xl md:w-auto font-semibold">SHEFA ISLAM FAHIM</h1>
               </div>
-              <div className="navbar-end">
-                <ul className="menu menu-horizontal px-1">
+              <div className="navbar-end lg:block hidden">
+                <ul className="menu menu-horizontal px-1 text-white">
                     {links}
                 </ul>
               </div>
