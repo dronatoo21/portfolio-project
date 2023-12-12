33e4e4e6159e47b3e5f934cf01f91768b6c1158f
dronatoo <mrdronatoo@gmail.com>
2023-12-11 17:38:09 -0800

    created download resume option

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,5 +1,7 @@
 import Lottie from "lottie-react";
 import programmer from "../../../assets/programmer.json"
+import { FaDownload } from "react-icons/fa";
+import resume from "../../../../Si_Fahim_Resume.pdf"
 
 const Banner = () => {
     return (
@@ -7,8 +9,13 @@ const Banner = () => {
             <div className="text-white md:pl-20 space-y-2 pb-14 text-center md:text-start">
                 <h1 className="md:text-3xl lg:text-5xl font-bold ">I AM FAHIM</h1>
                 <h3 className="md:text-2xl lg:text-3xl pb-5 md:pb-5 lg:pb-10">Front End Web Developer</h3>
-                <button className="btn bg-[#c8d5e5] text-[#3c3c3c] border-none">HIRE ME</button>
-                <a href="https://drive.google.com/file/d/1AcSJsV2ooH-nusPlN6QAuxNCZ-EWbJo6/view?usp=sharing"><button className="btn bg-[#fa3433] text-white border-none ml-3">RESUME</button></a>
+                <div className="flex gap-2">
+                    <div>
+                        <button className="btn bg-[#c8d5e5] text-[#3c3c3c] border-none">HIRE ME</button>
+                        <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1AcSJsV2ooH-nusPlN6QAuxNCZ-EWbJo6/view?usp=sharing"><button className="btn bg-[#fa3433] text-white border-none ml-3">RESUME</button></a>
+                    </div>
+                    <a href={resume} download={resume}><button className="bg-[#fa3433] h-6 pl-1 w-6 mt-5 rounded-[4px] hover:bg-slate-400"><FaDownload/></button></a>
+                </div>
             </div>
             <Lottie className="w-3/6" animationData={programmer}/>
         </div>
