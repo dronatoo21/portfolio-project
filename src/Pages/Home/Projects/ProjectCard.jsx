a1beca813ce64c0ab56a13bd6c4f089aafc578b9
dronatoo <mrdronatoo@gmail.com>
2024-05-21 16:41:42 -0700

    changed project theme to slider

--- src/Pages/Home/Projects/ProjectCard.jsx
+++ src/Pages/Home/Projects/ProjectCard.jsx
@@ -2,23 +2,23 @@ import { FaGithub, FaLink, FaServer } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const ProjectCard = ({project}) => {
-    const {_id, cover, title, description, languages, liveSite, clientCode, serverCode} = project
+    const {_id, cover, title, description, languages, liveSite, clientCode, serverCode} = project;
     return (
         <div className="text-white bg-[#31353b] rounded-xl">
-            <div className="card glass md:h-[600px] lg:h-[570px]">
-              <figure><img className="h-[250px] w-full rounded-lg" src={cover} alt="cover"/></figure>
+            <div className="card glass h-[520px] md:h-[530px] lg:h-[570px]">
+              <figure><img className="lg:h-[250px] h-[200px] w-full rounded-lg" src={cover} alt="cover"/></figure>
               <div className="card-body">
-                <h2 className="card-title text-2xl">{title}</h2>
+                <h2 className="card-title text-lg md:text-xl lg:text-2xl">{title}</h2>
                 {
-                    description?.length > 150 ? <p className="font-normal">{description?.slice(0, 150)} <Link to={`/details/${_id}`} className="font-bold text-blue-200">Read more...</Link></p> : <p>{description}</p>
+                    description?.length > 100 ? <p className="font-normal lg:text-base text-sm">{description?.slice(0, 100)} <Link to={`/details/${_id}`} className="font-bold text-blue-200">Read more...</Link></p> : <p className=" lg:text-base text-sm">{description}</p>
                 }
                 <hr className="w-1/2 mx-auto my-2"/>
-                <p className="font-semibold">{languages}</p>
+                <p className="font-semibold text-sm lg:text-base">{languages}</p>
                 <div className="flex justify-between">
                     <div className="items-center flex gap-3">
-                        <Link target="_blank" to={clientCode}><FaGithub className="text-2xl"/></Link>
-                        <Link target="_blank" to={serverCode}><FaServer className="text-2xl"/></Link>
-                        <Link target="_blank" to={liveSite}><FaLink className="text-2xl"/></Link>
+                        <Link target="_blank" to={clientCode}><FaGithub className="text-xl lg:text-2xl"/></Link>
+                        <Link target="_blank" to={serverCode}><FaServer className="text-xl lg:text-2xl"/></Link>
+                        <Link target="_blank" to={liveSite}><FaLink className="text-xl lg:text-2xl"/></Link>
                     </div>
                   <Link to={`/details/${_id}`}><button className="btn bg-[#c8d5e5] text-[#3c3c3c] border-none mt-2">Details</button></Link>
                 </div>
--- src/Pages/Home/Projects/Projects.jsx
+++ src/Pages/Home/Projects/Projects.jsx
@@ -1,5 +1,14 @@
 import { useEffect, useState } from "react";
 import ProjectCard from "./ProjectCard";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import required modules
+import { Pagination } from 'swiper/modules';
+// Import Swiper style
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
 const Projects = () => {
     const [projects, setProjects] = useState([])
     useEffect( () => {
@@ -12,12 +21,45 @@ const Projects = () => {
     return (
         <div id="projects" className="px-5 lg:px-0">
             <div>
-                <h1 className="font-bold text-3xl text-center text-white mb-2">Projects</h1>
+                <h1 className="font-bold text-xl md:text-xl lg:text-2xl text-center text-white mb-10">Projects</h1>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 mt-12">
+            {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 mt-12">
                 {
                     projects?.map(project => <ProjectCard key={project?._id} project={project}/>)
                 }
+            </div> */}
+            <div className="mx-2 md:mx-10">
+                <Swiper
+                slidesPerView={1}
+                spaceBetween={9}
+                pagination={{
+                  clickable: true,
+                }}
+                breakpoints={{
+                  640: {
+                    slidesPerView: 1,
+                    spaceBetween: 10,
+                  },
+                  768: {
+                    slidesPerView: 2,
+                    spaceBetween: 10,
+                  },
+                  1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 9,
+                  },
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+            {
+                projects?.map(project => (
+                    <SwiperSlide key={project?._id}>
+                        <ProjectCard project={project}/>
+                    </SwiperSlide>
+                ))
+            }
+                </Swiper>
             </div>
         </div>
     );
