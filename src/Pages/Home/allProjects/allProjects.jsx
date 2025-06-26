1adba80072f246e1dd7d05c6933c68f970156c84
dronatoo <mrdronatoo@gmail.com>
2025-06-26 08:38:09 +0600

    checking

--- src/Pages/Home/allProjects/allProjects.jsx
+++ src/Pages/Home/allProjects/allProjects.jsx
@@ -1,9 +1,9 @@
 const AllProjects = () => {
     return (
         <div>
-            <h1>Hi my name is fahim. and my favourite hobby is coding</h1>
+            <h1 className="text-4xl text-blue-300">Hi my name is fahim. and my favourite hobby is coding</h1>
         </div>
     );
 };
 
-export default AllProjects;
+export default AllProjects; 
